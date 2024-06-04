ddaaeb80df372cf6f45fd7719ca469eb80b52f20
dgmrtabu <dgmr.tabu@gmail.com>
2024-06-04 09:34:50 -0400

    Curso Sql - 3

---
+++ Writing/01_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (user_id, name, surname) 
+VALUES (7,'Maria', 'Lopez');
+
+INSERT INTO users (name, surname) 
+VALUES ('Paco', 'Perez');
+
+INSERT INTO users (user_id, name, surname) 
+VALUES (10,'El', 'Merch');
---
+++ Writing/02_update.sql
@@ -0,0 +1,4 @@
+
+UPDATE users 
+SET age = 21, name = 'El', surname = 'Merch', init_date = '2021-10-12' 
+WHERE user_id = 10
